3d9651b1bde390406172bfda6659b1dfcf909e70
seemur1 <77749403+seemur1@users.noreply.github.com>
2021-03-24 20:38:05 +0300

    Homework added
    В этом домаашнем задании я игрался с Array. методами + упарывался с циклами for, понимаю, что это - не по кодстайлу. А потому - в первый и последний раз))))

---
+++ Totoshka/Totoshka/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TotoshkaClasses;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Totoshka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool checker = true;
+            string[] buffer;
+            List<Street> streets = new List<Street>();
+
+            Console.WriteLine("Please, write the amount of streets to read!");
+            int.TryParse(Console.ReadLine(), out int amountOfStreets);
+            string[] lines = File.ReadAllLines("data.txt");
+            foreach (string line in lines)
+            {
+                buffer = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                // Проверяем, состоит ли строка из uint + по длине проверяем.
+                if ((buffer.Length <= 1) ||
+                    !Array.TrueForAll(buffer, (string value) => (value == buffer[0]) ? true : uint.TryParse(value, out uint useless)))
+                {
+                    checker = false;
+                }
+            }
+            // Если в строчках мусор - заполняем список рандомно сгенерированными улицами.
+            if (!checker)
+            {
+                for (; streets.Count < amountOfStreets; streets.Add(new Street())) { }
+            }
+            // Иначе - заполняем список улицами, предусмотрительно ограничив счётчик улиц.
+            else
+            {
+                amountOfStreets = Math.Min(amountOfStreets, lines.Length);
+                for (; streets.Count < amountOfStreets; streets.Add(new Street(lines[streets.Count].Split(' ', StringSplitOptions.RemoveEmptyEntries)))) { }
+            }
+            Array.ForEach(streets.ToArray(), Console.WriteLine);
+            // Выводим все строчки в файл.
+            File.WriteAllLines("out.txt", Array.ConvertAll(streets.ToArray(), (Street street) => street.pureString()));
+        }
+    }
+}
---
+++ Totoshka/TotoshkaRecovery/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TotoshkaClasses;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TotoshkaRecovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Инициализирум массив улиц.
+            List<Street> streets = new List<Street>();
+
+            string[] lines = File.ReadAllLines("./../../../../Totoshka/bin/Debug/netcoreapp3.1/out.txt");
+            // Заполняем массив улиц.
+            for (; streets.Count < lines.Length; streets.Add(new Street(lines[streets.Count].Split(' ', StringSplitOptions.RemoveEmptyEntries)))) { }
+            // Выводим содержимое на экран:
+            Array.ForEach(streets.ToArray(), (Street street) => Console.Write((~street % 2 == 0) && (+street) ? street.ToString() + "\n" : ""));
+            Console.WriteLine("All such streets were written!");
+        }
+    }
+}
